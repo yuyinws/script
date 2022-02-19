ba0e94becee438198f2a94e1d79cb06059ae4c2e
yuyinws <lyc657508366@gmail.com>
2022-02-19 14:08:15 +0800

    ✨ feat(keylol): 历史记录

--- keylol/history.js
+++ keylol/history.js
@@ -4,124 +4,117 @@
 // @version      0.1
 // @description  add history feature for Keylol
 // @author       yuyinws
-// @include      https://keylol.com/t*
+// @include      https://keylol.com/*
 // @icon         https://keylol.com/favicon.ico
 // @grant        unsafeWindow
 // ==/UserScript==
-class DB {
-  constructor() {
-    this.initDB()
-  }
 
-  initDB() {
-    let request = window.indexedDB.open('history_view')
-    request.onsuccess = (event) => {
-      this.db = event.target.result
-      console.log('数据库打开成功')
-    }
-    request.onupgradeneeded = function (event) {
-      this.db = event.target.result
-      var objectStore
-      if (!this.db.objectStoreNames.contains('history')) {
-        objectStore = this.db.createObjectStore('history', {
-          keyPath: 'id',
-          autoIncrement: true,
-        })
-        objectStore.createIndex('titleIndex', 'tid', { unique: false })
-      }
-      console.log('数据库升级成功')
+class History {
+  constructor(url) {
+    this.history = JSON.parse(window.localStorage.getItem('history')) || []
+    if (url.indexOf('https://keylol.com/t') > -1) {
+      this.tID = url.split('t')[3].split('-')[0]
+      this.tTitle = document.querySelector('#thread_subject').title
+      this.add()
     }
   }
 
-  async addHistory(tid, title) {
-    try {
-      const findOne = await this.findOneByTID(tid)
-      console.log(findOne)
-      if (findOne) {
-        await this.deleteOneByID(findOne.id)
-      }
-      this.addOne(tid, title)
-    } catch (error) {
-      console.log(error)
-    }
-  }
-
-  async addOne(tid, title) {
-    return new Promise((resolve, reject) => {
-      let request = this.db
-        .transaction(['history'], 'readwrite')
-        .objectStore('history')
-        .add({
-          tid: tid,
-          title: title,
-        })
-      request.onsuccess = (event) => {
-        resolve(event)
-        console.log('数据写入成功')
-      }
-
-      request.onerror = (event) => {
-        reject(event)
-        console.log('数据写入失败', event)
+  add() {
+    this.history.forEach((item, index) => {
+      if (item.tID === this.tID) {
+        this.history.splice(index, 1)
+        return
       }
     })
-  }
 
-  async deleteOneByID(id) {
-    return new Promise((resolve, reject) => {
-      let request = this.db
-        .transaction(['history'], 'readwrite')
-        .objectStore('history')
-        .delete(id)
-
-      request.onsuccess = (event) => {
-        resolve(event)
-        console.log('数据删除成功')
-      }
-      request.onerror = (error) => {
-        reject(error)
-      }
+    this.history.unshift({
+      tID: this.tID,
+      tTitle: this.tTitle,
     })
+
+    window.localStorage.setItem('history', JSON.stringify(this.history))
   }
 
-  async findOneByTID (tid) {
-    let transaction = this.db.transaction(['history'])
-    let objectStore = transaction.objectStore('history')
-    let index = objectStore.index('titleIndex')
-    let request = index.get(tid)
-    return new Promise((resolve, reject) => {
-      request.onsuccess = (event) => {
-        resolve(event.target.result)
-      }
-      request.onerror = (event) => {
-        reject(event)
-      }
-    })
-  }
-
-  async readAll () {
-    let objectStore = this.db.transaction('history_view').objectStore('history')
-    objectStore.openCursor().onsuccess = (event) => {
-      let cursor = event.target.result;
-      if (cursor) {
-        console.log(cursor)
-        cursor.continue()
-      } else {
-        console.log('没有更多数据了！');
-      }
-    }
+  get() {
+    return this.history
   }
 }
 
-;(function () {
-  'use strict'
-  const tID = window.location.pathname.split('t')[1].split('-')[0]
-  const title = document.querySelector('#thread_subject').title
-  console.log(tID, title)
-  let db = new DB()
-  setTimeout(() => {
-    db.addHistory(tID, title)
-    db.readAll()
-  }, 3000)
-})()
+const render = (_hisList) => {
+  let ref = document.querySelector('.list-inline').childNodes[4]
+  let father = document.querySelector('.list-inline')
+  let historyDom = document.createElement('div')
+  let historyCard = document.createElement('div')
+  let clearHistory = document.createElement('button')
+  clearHistory.id = 'clearHistory'
+  clearHistory.innerHTML = '清除'
+  clearHistory.style.margin = '4px 0 4px 5px'
+  clearHistory.style.padding = '4px 8px'
+  clearHistory.style.background = '#74bcff'
+  clearHistory.style.border = 'none'
+  clearHistory.style.color = '#fff'
 
+  historyDom.style.position = 'relative'
+  historyDom.id = 'history'
+  historyDom.style.height = '30px'
+  historyDom.style.lineHeight = '27px'
+  historyDom.style.display = 'inline-block'
+  historyDom.style.padding = '0 10px'
+
+  historyCard.id = 'historyCard'
+  historyCard.style.width = '200px'
+  historyCard.style.minHeight = '50px'
+  historyCard.style.maxHeight = '200px'
+  historyCard.style.overflowY = 'auto'
+  historyCard.style.border = '1px solid #D1D5DB'
+  historyCard.style.position = 'absolute'
+  historyCard.style.left = '-70px'
+  historyCard.style.zIndex = '999'
+  historyCard.style.backgroundColor = '#fff'
+  historyCard.style.display = 'none'
+
+  historyDom.innerHTML =
+    '<span id="historyText" style="cursor: pointer;">历史记录</span>'
+  father.insertBefore(historyDom, ref)
+  historyDom.appendChild(historyCard)
+  historyCard.appendChild(clearHistory)
+
+  historyDom.addEventListener('mouseover', () => {
+    historyCard.style.display = 'block'
+  })
+  historyDom.addEventListener('mouseout', () => {
+    historyCard.style.display = 'none'
+  })
+
+  let historyText = document.querySelector('#historyText')
+  historyText.addEventListener('mouseover', () => {
+    historyDom.style.backgroundColor = '#74bcff'
+    historyText.style.color = '#fff'
+  })
+  historyText.addEventListener('mouseout', () => {
+    historyDom.style.backgroundColor = '#f7f7f7'
+    historyText.style.color = '#000'
+  })
+
+  clearHistory.addEventListener('click', () => {
+    window.localStorage.removeItem('history')
+    historyCard.innerHTML = ''
+  })
+
+  _hisList.forEach((item) => {
+    let historyCardItem = document.createElement('div')
+    historyCardItem.style.padding = '5px'
+    historyCardItem.style.cursor = 'pointer'
+    historyCardItem.style.borderBottom = '1px solid #D1D5DB'
+    historyCardItem.style.fontSize = '12px'
+    historyCardItem.style.color = '#000'
+    historyCardItem.style.lineHeight = '20px'
+    historyCardItem.innerHTML = `<a id="aTag" href="https://keylol.com/t${item.tID}-1-1">${item.tTitle}</a>`
+    historyCard.appendChild(historyCardItem)
+  })
+}
+
+;(() => {
+  const history = new History(window.location.href)
+  render(history.get())
+})()
